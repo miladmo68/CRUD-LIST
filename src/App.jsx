0a4ad9de80ee2f4683e2f379e134e54ba2ebc86f
miladmo68 <miladmo68@gmail.com>
2025-12-18 12:46:54 -0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import { useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const initialItems = [
+    { id: 1, text: "Buy milk" },
+    { id: 2, text: "Finish CRUD task" },
+    { id: 3, text: "Go to gym" },
+    { id: 4, text: "Read 10 pages" },
+    { id: 5, text: "Call the clinic" },
+  ];
 
-  return <div className=" flex flex-col items-center ">hi</div>;
+  const [items, setItems] = useState(initialItems);
+
+  // setItems((prev) => [...prev, initialItems]);
+
+  return (
+    <div className=" flex flex-col items-center ">
+      <h1>CURD LIST</h1>
+      <div>
+        {items.length > 0 ? (
+          <ol>
+            {items.map((item) => (
+              <li key={item.id}>{item.text}</li>
+            ))}
+          </ol>
+        ) : (
+          <p>No List Available</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
